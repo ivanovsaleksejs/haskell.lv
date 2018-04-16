e51b6c70b26b5dfadabf9d3a56d9e2ecdc5fc3ba
Aleksejs Ivanovs <ivanovs.aleksejs@gmail.com>
2018-04-16 14:05:08 +0300

    Revert "Brush up code a little and move users into users.nix."
    This reverts commit 61c6aafee6ce376f6f15acb249aa580a59590605.

--- fair.nix
+++ fair.nix
@@ -1,26 +1,43 @@
 {
   network.description = "NixOS droplet";
 
-  webserver = { config, pkgs, ... }: {
-    imports = [
-      ./fair-networking.nix # generated at runtime by nixos-infect
-      ./users.nix
-    ];
+  webserver =
+    { config, pkgs, ... }:
+    { 
+      imports = [
+       ./fair-networking.nix # generated at runtime by nixos-infect
+      ];
 
-    boot.cleanTmpDir = true;
-    networking.hostName = "NixOS";
-    networking.firewall.allowPing = true;
-
-    services = {
-      nginx.enable = true;
-      openssh = {
+      boot.cleanTmpDir = true;
+      networking.hostName = "NixOS";
+      networking.firewall.allowPing = true;
+      services.nginx.enable = true;
+      services.openssh = {
         enable = true;
         permitRootLogin = "yes";
       };
+      users.users = {
+        root = {
+          openssh.authorizedKeys.keys = [
+         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdQTg3/08c0FeapBUcsypdHc57Mjx3P0zBuuUVcfg//s8Y7gYt02Rjli7meBqf34TOc5AbARkX32B8OaWHLw4UZ9u1WAkOJ6xAROmWTh+hR0poGDcGtIhCUgXCniaJ7WfoASX0EZF5L6GfrCXAF+uqCohSd2SfGW+tIusf8nhAbxGr+p/5Dk8FfB/QUZvmsmdHV6aJmSUACDcOc9qh+Lks2POcpj3xukFdv6LC4C/E2q227hfvK2+rIuV0RNTyTIzPBZD1TZen66p1Ty4eDP+lvsVJaarTYkdpb1wwKiYZzfxLAqLD9eJa2TifgZKhTg4zZbBJEgBva9ujpwzUh7tn aleksejs@nixos"
+       ];
+     };
+        aleksejs = {
+          isSystemUser = true;
+          createHome = true;
+          shell = pkgs.zsh;
+          openssh.authorizedKeys.keys = [
+            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdQTg3/08c0FeapBUcsypdHc57Mjx3P0zBuuUVcfg//s8Y7gYt02Rjli7meBqf34TOc5AbARkX32B8OaWHLw4UZ9u1WAkOJ6xAROmWTh+hR0poGDcGtIhCUgXCniaJ7WfoASX0EZF5L6GfrCXAF+uqCohSd2SfGW+tIusf8nhAbxGr+p/5Dk8FfB/QUZvmsmdHV6aJmSUACDcOc9qh+Lks2POcpj3xukFdv6LC4C/E2q227hfvK2+rIuV0RNTyTIzPBZD1TZen66p1Ty4eDP+lvsVJaarTYkdpb1wwKiYZzfxLAqLD9eJa2TifgZKhTg4zZbBJEgBva9ujpwzUh7tn aleksejs@nixos"
+          ];
+        };
+        s = {
+          isSystemUser = true;
+          createHome = true;
+          shell = pkgs.zsh;
+          openssh.authorizedKeys.keys = [
+            "sh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDwHcSSm2TdzOwXEG6BLdhjOtHKHect9NIHn3uFm4i32irEBE89uSeZomVjOBYppFaGhKiyTGm+BsHXUq79lMZwQfZTjJyJMUwBE3is8M3FmuF0oBSsjtIYZM5tDOfBX12EuWjxrPDwzqQnCY95RXs05BljxvsBEoDBnUyYR4jbszzU+Xtk9/UdUGUnabGwvbQxIEd0JuOljOz2NNa5pWc8KwlqoqLdOeaIHMDMRnEt9bmGZjiVhkYPdxJfvxL+x4LfdSeidOaQhVRgFGmG5wCrj2/nQWffKL+hGaBSzPcy49qWTM/P2232FTkayCP1pBediE/iewmliaBFsFAFSO8x ij@self"
+          ];
+        };
+      };
     };
-
-    environment.systemPackages = with pkgs; [
-      vim
-    ];
-  };
 }
